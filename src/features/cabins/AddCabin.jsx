7431bbe716f3de6354c83ab2ee7b7793215543c5
Bogomil Petrov <bogipetrov@gmail.com>
2024-04-04 22:54:19 +0300

    Building modal window using React portal

---
+++ src/features/cabins/AddCabin.jsx
@@ -0,0 +1,45 @@
+import Button from "../../ui/Button";
+import Modal from "../../ui/Modal";
+import CreateCabinForm from "./CreateCabinForm";
+import CabinTable from "./CabinTable";
+
+function AddCabin() {
+  return (
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
+  );
+}
+
+export default AddCabin;
+
+// function AddCabin() {
+//   const [isOpenModel, setIsOpenModel] = useState(false);
+
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModel((show) => !show)}>
+//         Add new cabin
+//       </Button>
+//       {isOpenModel && (
+//         <Modal onClose={() => setIsOpenModel(false)}>
+//           <CreateCabinForm onCloseModal={() => setIsOpenModel(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default AddCabin;
